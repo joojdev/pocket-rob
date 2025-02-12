35422908bff972ea0742083fd59143b6892df563
joojdev <jvpiovezan2905@gmail.com>
2025-02-12 10:36:50 -0300

    Fixed some code

--- main.js
+++ main.js
@@ -1,207 +1,216 @@
-const playButton = document.querySelector('#playButton')
+const playButton = document.querySelector('#playButton');
 
 playButton.addEventListener('click', async () => {
-  if (playing) return
-  playing = true
-  playButton.textContent = '⌛'
-  const code = javascript.javascriptGenerator.workspaceToCode(workspace)
-  await eval(code)
-  playButton.textContent = '▶'
-  playing = false
-})
+  if (playing) return;
+  playing = true;
+  playButton.textContent = '⌛';
+  const code = javascript.javascriptGenerator.workspaceToCode(workspace);
+  await eval(code);
+  playButton.textContent = '▶';
+  playing = false;
+});
 
-const canvasElement = document.querySelector('#gameScreen')
-const gameScreen = canvasElement.getContext('2d')
+const canvasElement = document.querySelector('#gameScreen');
+const gameScreen = canvasElement.getContext('2d');
 
-let spritesLoaded = 0
+let spritesLoaded = 0;
 
-let spriteList = ['background', 'rob-sprite', 'sombra', 'aviso']
+let spriteList = ['background', 'rob-sprite', 'sombra', 'aviso'];
 
 spriteList = spriteList.map((file) => {
-  const image = new Image()
-  image.src = `./sprites/${file}.png`
+  const image = new Image();
+  image.src = `./sprites/${file}.png`;
 
   image.onload = () => {
-    spritesLoaded += 1
+    spritesLoaded += 1;
 
     if (spritesLoaded >= spriteList.length) {
-      draw()
+      draw();
     }
-  }
+  };
 
-  return image
-})
+  return image;
+});
 
-let x = 1
-let y = 2
-let facing = 'front'
-let playing = false
+let x = 1;
+let y = 2;
+let facing = 'front';
+let playing = false;
+let holdingRob = false;
 
-canvasElement.addEventListener('click', (event) => {
-  const rectangle = canvasElement.getBoundingClientRect()
-  const clickX = event.offsetX
-  const clickY = event.offsetY
+canvasElement.addEventListener('mousedown', (event) => {
+  const clickX = event.offsetX;
+  const clickY = event.offsetY;
 
-  const minX = ((x * 16) + x + 1) * 4
-  const maxX = ((x * 16) + x + 17) * 4
-  const minY = ((y * 16) + y + 1) * 4
-  const maxY = ((y * 16) + y + 17) * 4
+  const minX = ((x * 16) + x + 1) * 4;
+  const maxX = ((x * 16) + x + 17) * 4;
+  const minY = ((y * 16) + y + 1) * 4;
+  const maxY = ((y * 16) + y + 17) * 4;
 
   if (clickX >= minX && clickX <= maxX && clickY >= minY && clickY <= maxY) {
-    console.log('hello, world!')
+    holdingRob = true;
   }
-})
+});
+
+canvasElement.addEventListener('mouseup', (event) => {
+  if (holdingRob) {
+    holdingRob = false;
+
+    const clickX = event.offsetX;
+    const clickY = event.offsetY;
+  }
+});
 
 function drawRob(drawX, drawY, facing, snapToGrid = true, surprised = false) {
-  if ((drawX > 6 || drawY > 6 || drawX < 0 || drawY < 0) && snapToGrid) return
+  if ((drawX > 6 || drawY > 6 || drawX < 0 || drawY < 0) && snapToGrid) return;
   
-  let posX = drawX
-  let posY = drawY
+  let posX = drawX;
+  let posY = drawY;
   
   if (snapToGrid) {
-    posX = (drawX * 16) + drawX + 1
-    posY = (drawY * 16) + drawY + 1
+    posX = (drawX * 16) + drawX + 1;
+    posY = (drawY * 16) + drawY + 1;
   }
-  let spriteX = 0
-  let spriteY = 0
+  let spriteX = 0;
+  let spriteY = 0;
 
   switch (facing) {
     case 'back':
-      spriteY = 16
-      break
+      spriteY = 16;
+      break;
     case 'front':
-      spriteY = 0
-      break
+      spriteY = 0;
+      break;
     case 'left':
-      spriteY = 32
-      break
+      spriteY = 32;
+      break;
     case 'right':
-      spriteY = 48
-      break
+      spriteY = 48;
+      break;
   }
-  gameScreen.drawImage(spriteList[2], posX, posY)
-  gameScreen.drawImage(spriteList[1], spriteX, spriteY, 16, 16, posX, posY, 16, 16)
-  if (!surprised) return
-  gameScreen.drawImage(spriteList[3], posX, posY - 3)
+  gameScreen.drawImage(spriteList[2], posX, posY);
+  gameScreen.drawImage(spriteList[1], spriteX, spriteY, 16, 16, posX, posY, 16, 16);
+  if (!surprised) return;
+  gameScreen.drawImage(spriteList[3], posX, posY - 3);
 }
 
 function clear() {
-  gameScreen.drawImage(spriteList[0], 0, 0)
+  gameScreen.drawImage(spriteList[0], 0, 0);
 }
 
 function draw(snapToGrid = true, surprised = false) {
-  clear()
-  drawRob(x, y, facing, snapToGrid, surprised)
+  clear();
+  drawRob(x, y, facing, snapToGrid, surprised);
 }
 
-const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function turn(direction) {
   if (direction == 'left') {
     switch (facing) {
       case 'back':
-        facing = 'left'
-        break
+        facing = 'left';
+        break;
       case 'front':
-        facing = 'right'
-        break
+        facing = 'right';
+        break;
       case 'left':
-        facing = 'front'
-        break
+        facing = 'front';
+        break;
       case 'right':
-        facing = 'back'
-        break
+        facing = 'back';
+        break;
     }
   } else if (direction == 'right') {
     switch (facing) {
       case 'back':
-        facing = 'right'
-        break
+        facing = 'right';
+        break;
       case 'front':
-        facing = 'left'
-        break
+        facing = 'left';
+        break;
       case 'left':
-        facing = 'back'
-        break
+        facing = 'back';
+        break;
       case 'right':
-        facing = 'front'
-        break
+        facing = 'front';
+        break;
     }
   }
 
-  draw()
-  await delay(500)
+  draw();
+  await delay(500);
 }
 
 async function walk(blocks) {
-  let xOffset = 0
-  let yOffset = 0
+  let xOffset = 0;
+  let yOffset = 0;
 
   switch (facing) {
     case 'back':
-      yOffset = -1
-      break
+      yOffset = -1;
+      break;
     case 'front':
-      yOffset = 1
-      break
+      yOffset = 1;
+      break;
     case 'left':
-      xOffset = -1
-      break
+      xOffset = -1;
+      break;
     case 'right':
-      xOffset = 1
-      break
+      xOffset = 1;
+      break;
   }
 
-  const xForecast = x + (xOffset * blocks)
-  const yForecast = y + (yOffset * blocks)
-  let surprised = false
+  const xForecast = x + (xOffset * blocks);
+  const yForecast = y + (yOffset * blocks);
+  let surprised = false;
   
   if (xForecast > 6) {
-    blocks -= xForecast - 6
-    surprised = true
+    blocks -= xForecast - 6;
+    surprised = true;
   } else if (yForecast > 6) {
-    blocks -= yForecast - 6
-    surprised = true
+    blocks -= yForecast - 6;
+    surprised = true;
   } else if (xForecast < 0) {
-    blocks += xForecast
-    surprised = true
+    blocks += xForecast;
+    surprised = true;
   } else if (yForecast < 0) {
-    blocks += yForecast
-    surprised = true
+    blocks += yForecast;
+    surprised = true;
   }
 
   for (let i = 0; i < blocks; i++) {
-    let oldX = x
-    let oldY = y
+    let oldX = x;
+    let oldY = y;
 
-    x = (x * 16) + x + 1
-    y = (y * 16) + y + 1
+    x = (x * 16) + x + 1;
+    y = (y * 16) + y + 1;
 
     for (let j = 0; j < 16; j ++) {
-      x += xOffset
-      y += yOffset
-      draw(false)
-      await delay(50)
+      x += xOffset;
+      y += yOffset;
+      draw(false);
+      await delay(50);
     }
 
-    x = oldX + xOffset
-    y = oldY + yOffset
-    draw()
-    await delay(500)
+    x = oldX + xOffset;
+    y = oldY + yOffset;
+    draw();
+    await delay(500);
   }
 
   if (surprised) {
-    draw(true, true)
-    await delay(500)
-    draw()
-    await delay(500)
-    draw(true, true)
-    await delay(500)
-    draw()
-    await delay(500)
-    draw(true, true)
-    await delay(500)
-    draw()
-    await delay(500)
+    draw(true, true);
+    await delay(300);
+    draw();
+    await delay(300);
+    draw(true, true);
+    await delay(300);
+    draw();
+    await delay(300);
+    draw(true, true);
+    await delay(300);
+    draw();
+    await delay(300);
   }
 }
